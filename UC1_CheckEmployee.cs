3394f1bc99ecb9208b5db173cb438f5566b70c4c
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-16 23:44:16 +0530

    uc1

---
+++ UC1_CheckEmployee.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace day10_Employee_Wages_Computation
+{
+    class UC1_EmpCheck
+    {
+        public static void CheckEmployee()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee Is Present");
+            else
+                Console.WriteLine("Employee Is Absent");
+        }
+    }
+}
